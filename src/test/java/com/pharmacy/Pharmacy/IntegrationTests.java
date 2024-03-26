fb750ea04c25ca785b50ba9af883a865fc390f62
andrzej512 <andrzej512@onet.eu>
2024-03-26 16:39:58 +0100

    - add simple integration api test

--- src/test/java/com/pharmacy/Pharmacy/IntegrationTests.java
+++ src/test/java/com/pharmacy/Pharmacy/IntegrationTests.java
@@ -1,37 +1,37 @@
 package com.pharmacy.Pharmacy;
+import com.pharmacy.Pharmacy.controller.PharmacyBranchController;
+import com.pharmacy.Pharmacy.service.PharmacyBranchService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(PharmacyBranchController.class)
 public class IntegrationTests {
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mvc;
+    @MockBean
+    PharmacyBranchService pharmacyBranchService;
 
     @Test
-    public void shouldReturnExpectedText() throws Exception {
-        mockMvc
-                .perform(get("/localhost:8080/api/v1/pharmacies"))
+    public void addEmployeeTest() throws Exception {
+
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/pharmacyBranch")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello!")));
+                .andExpect(status().isOk());
     }
 }
 
