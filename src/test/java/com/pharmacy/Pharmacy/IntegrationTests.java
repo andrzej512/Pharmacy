c6d23b5d98881e69a5ec8216fb70acbbbf5c5b84
andrzej512 <andrzej512@onet.eu>
2024-03-26 13:23:44 +0100

    - add integration test file - delete spy annotation

---
+++ src/test/java/com/pharmacy/Pharmacy/IntegrationTests.java
@@ -0,0 +1,37 @@
+package com.pharmacy.Pharmacy;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class IntegrationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnExpectedText() throws Exception {
+        mockMvc
+                .perform(get("/localhost:8080/api/v1/pharmacies"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello!")));
+    }
+}
+
