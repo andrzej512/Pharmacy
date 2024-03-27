84a6955978a264fd12a5267d37ddcc840252ddca
andrzej512 <andrzej512@onet.eu>
2024-03-27 13:26:31 +0100

    - move test data generation to other class

--- src/test/java/com/pharmacy/Pharmacy/PharmacyServiceTests.java
+++ src/test/java/com/pharmacy/Pharmacy/PharmacyServiceTests.java
@@ -2,6 +2,8 @@ package com.pharmacy.Pharmacy;
 
 import com.pharmacy.Pharmacy.dto.PharmacyBranchDTO;
 import com.pharmacy.Pharmacy.exceptions.ResourceNotFoundException;
+import com.pharmacy.Pharmacy.mappers.PharmacyBranchDTOMapper;
+import com.pharmacy.Pharmacy.mappers.PharmacyBranchMapper;
 import com.pharmacy.Pharmacy.model.Pharmacy;
 import com.pharmacy.Pharmacy.model.PharmacyBranch;
 import com.pharmacy.Pharmacy.model.PharmacyBranchAddress;
@@ -9,14 +11,17 @@ import com.pharmacy.Pharmacy.repository.PharmacyBranchRepository;
 import com.pharmacy.Pharmacy.repository.PharmacyRepository;
 import com.pharmacy.Pharmacy.service.PharmacyBranchService;
 import com.pharmacy.Pharmacy.service.PharmacyService;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 import org.mockito.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.*;
 
 import org.junit.Before;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +30,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PharmacyServiceTests {
+    TestData testData = new TestData();
     @Mock
     PharmacyRepository pharmacyRepository;
     @Mock
@@ -33,35 +39,6 @@ public class PharmacyServiceTests {
     PharmacyService pharmacyService;
     @InjectMocks
     PharmacyBranchService pharmacyBranchService;
-    List<Pharmacy> pharmacies = Arrays.asList(
-            Pharmacy.builder().id(1L).name("PharmacyTest1").build(),
-            Pharmacy.builder().id(2L).name("PharmacyTest2").build());
-    Pharmacy pharmacy = Pharmacy.builder().id(8L).name("PharmacyNameTest").build();
-    List<PharmacyBranch> pharmacyBranches = Arrays.asList(
-            PharmacyBranch.builder()
-                    .id(1L)
-                    .pharmacy(pharmacies.get(0))
-                    .pharmacyBranchAddress(PharmacyBranchAddress.builder()
-                            .id(1L)
-                            .city("testCity")
-                            .street("testStreet")
-                            .country("testCountry").build()).build(),
-            PharmacyBranch.builder()
-                    .id(2L)
-                    .pharmacy(pharmacies.get(1))
-                    .pharmacyBranchAddress(PharmacyBranchAddress.builder()
-                            .id(2L)
-                            .city("testCity2")
-                            .street("testStreet2")
-                            .country("testCountry2").build()).build());
-
-    PharmacyBranchDTO expectedPharmacyBranchDTO = PharmacyBranchDTO
-            .builder()
-            .pharmacyCountry(pharmacyBranches.get(0).getPharmacyBranchAddress().getCountry())
-            .pharmacyCity(pharmacyBranches.get(0).getPharmacyBranchAddress().getCity())
-            .pharmacyStreet(pharmacyBranches.get(0).getPharmacyBranchAddress().getStreet())
-            .pharmacyName(pharmacyBranches.get(0).getPharmacy().getName())
-            .build();
 
     @Before
     public void setup() {
@@ -71,21 +48,21 @@ public class PharmacyServiceTests {
     @Test
     public void testGetPharmacyId() {
         given(pharmacyRepository.findOneByName("PharmacyNameTest"))
-                .willReturn(Optional.ofNullable(pharmacy));
-        assertThat(pharmacyService.getPharmacyId("PharmacyNameTest")).isEqualTo(8L);
+                .willReturn(Optional.ofNullable(testData.pharmacies.get(0)));
+        assertThat(pharmacyService.getPharmacyId("PharmacyNameTest")).isEqualTo(1L);
         assertThrows(ResourceNotFoundException.class, () -> pharmacyService.getPharmacyId("PharmacyNameNonExistingo"));
     }
 
     @Test
     public void testGetPharmaciesWhenPharmaciesExist() {
-        given(pharmacyRepository.findAll()).willReturn(pharmacies);
+        given(pharmacyRepository.findAll()).willReturn(testData.pharmacies);
         assertThat(pharmacyService.getPharmacies().size()).isEqualTo(2);
     }
 
     @Test
     public void testGetPharmacyBranchWhenCountriesNull() {
-        given(pharmacyBranchRepository.findAll()).willReturn(pharmacyBranches);
+        given(pharmacyBranchRepository.findAll()).willReturn(testData.pharmacyBranches);
         assertThat(pharmacyBranchService.getPharmacyBranch(null).size()).isEqualTo(2);
-        assertThat(pharmacyBranchService.getPharmacyBranch(null).get(0)).isEqualTo(expectedPharmacyBranchDTO);
+        assertThat(pharmacyBranchService.getPharmacyBranch(null).get(0)).isEqualTo(testData.expectedPharmacyBranchDTO);
     }
 }
